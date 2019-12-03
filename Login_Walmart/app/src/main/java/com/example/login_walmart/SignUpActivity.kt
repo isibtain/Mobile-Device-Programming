673ca6de49d14a9e40643363f961aa6a6f960079
Sibtain Raza <47073093+isibtain@users.noreply.github.com>
2019-12-02 21:32:41 -0600

    Walmart Login App
    Simple App for Walmart shopping

---
+++ Login_Walmart/app/src/main/java/com/example/login_walmart/SignUpActivity.kt
@@ -0,0 +1,15 @@
+package com.example.login_walmart
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.shopping_activity.*
+
+class SignUpActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.singup_activity)
+
+    }
+
+}
